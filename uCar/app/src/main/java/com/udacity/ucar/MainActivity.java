3e5085e8a8c86f10510b669a9b21b2881800c720
Gustavo Cevallos <gstavocevallos@gmail.com>
2016-11-13 16:07:30 -0500

    Add the code of the conecction and service

--- uCar/app/src/main/java/com/udacity/ucar/MainActivity.java
+++ uCar/app/src/main/java/com/udacity/ucar/MainActivity.java
@@ -1,5 +1,9 @@
 package com.udacity.ucar;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -11,6 +15,7 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +34,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Reciber Message
+        IntentFilter filter = new IntentFilter(SocketService.ACTION_RECIBER);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        receiver = new ResponseReceiver();
+        registerReceiver(receiver, filter);
+
+        Intent msgIntent = new Intent(this, SocketService.class);
+        startService(msgIntent);
+
+        //Sender Message
+        IntentFilter filter2 = new IntentFilter(SocketService.ACTION_SEND);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        sender = new ResponseSender();
+        registerReceiver(sender, filter);
+
+        Intent msgIntent2 = new Intent(this, SocketService.class);
+        msgIntent.putExtra(SocketService.PARAM_IN_MSG, "json");
+        startService(msgIntent);
 
         mLvMenu = (ListView) findViewById(R.id.lv_menu);
 
@@ -97,4 +120,20 @@ public class MainActivity extends AppCompatActivity {
 
         return items;
     }
+
+    public class ResponseReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra(SocketService.PARAM_OUT_MSG);
+            Toast toast = Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+    public class ResponseSender extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra(SocketService.PARAM_OUT_MSG);
+            Toast toast = Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT);
+            toast.show();
+        }
 }
