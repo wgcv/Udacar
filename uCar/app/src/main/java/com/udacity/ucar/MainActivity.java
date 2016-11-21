d151b864ede94a67ea03c0124aa4e4cd590e6b5e
Saul Mestanza <saul.mm92@gmail.com>
2016-11-20 23:03:23 -0500

    Finishing layout3

--- uCar/app/src/main/java/com/udacity/ucar/MainActivity.java
+++ uCar/app/src/main/java/com/udacity/ucar/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -31,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
   /*    //Reciber Message
         IntentFilter filter = new IntentFilter(SocketServiceReciber.ACTION_RECIBER);
